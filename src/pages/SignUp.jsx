332aa2a47c7c664ea2596976f4e46ec36c8265b0
Adesojisouljay <adesojiisrael@gmail.com>
2024-11-16 21:45:45 +0100

    remove local host url

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -95,7 +95,7 @@ export const SignUp = () => {
   const getAccountKeys = async (username) => {
     setLoading(true);
     try {
-        const response = await axios.post('http://localhost:4000/get-account-keys', { username });
+        const response = await axios.post('https://api.breakaway.community/get-account-keys', { username });
         console.log("Full response:", response.data);
 
         if (response.data && response.data.accountDetails) {
